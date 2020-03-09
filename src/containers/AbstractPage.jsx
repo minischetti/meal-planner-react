35511dff1b40a4cab8047e590bdce2ad30c0fd2c
Dominic Minischetti <domminischetti@gmail.com>
2020-03-09 15:02:38 -0700

    Add support for full-width content when only one child is passed to the page component.

--- src/containers/AbstractPage.jsx
+++ src/containers/AbstractPage.jsx
@@ -4,7 +4,7 @@ import { GlobalHeader } from "../components/GlobalHeader";
 import { PAGE_SECTION_AREA } from "../components/ui/page";
 
 export const AbstractPage = ({ children, disableGlobalHeader = false }) => {
-    const pageStyle = css`
+    const container = css`
         font-family: "Lato", sans-serif;
         transition: 0.5s all ease-in-out;
         display: grid;
@@ -12,21 +12,37 @@ export const AbstractPage = ({ children, disableGlobalHeader = false }) => {
         gap: 20px;
     `;
 
-    const pageContentStyle = css`
+    const content = css`
         display: grid;
         gap: 20px;
-        grid-template-columns: 1fr 2fr 1fr;
-        grid-template-areas: "${PAGE_SECTION_AREA.LEFT} ${PAGE_SECTION_AREA.MAIN} ${PAGE_SECTION_AREA.MAIN}";
+        grid-template-areas: "${PAGE_SECTION_AREA.MAIN}";
         @media (min-width: 375px) {
             width: 60%;
             margin: 0 auto;
         }
     `;
 
+    const trifold = css`
+        grid-template-columns: 1fr 2fr 1fr;
+        grid-template-areas: "${PAGE_SECTION_AREA.LEFT} ${PAGE_SECTION_AREA.MAIN} ${PAGE_SECTION_AREA.MAIN}";
+    `;
+
+    const getContentStyles = () => {
+        const styles = [content];
+
+        if (children.length > 1) {
+            styles.push(trifold);
+        }
+
+        console.log(styles);
+
+        return styles;
+    }
+
     return (
-        <div css={pageStyle}>
+        <div css={container}>
             {disableGlobalHeader ? null : <GlobalHeader />}
-            <div css={pageContentStyle}>{children}</div>
+            <div css={getContentStyles()}>{children}</div>
         </div>
     );
 };
